d468d4f86bf865599a0c159ff05ddbbacf56a1e5
smorrissey <64bitart@gmail.com>
2017-09-18 12:50:44 -0700

    alligned map and added mr v.'s tutorial

---
+++ rofllmao/Assets/ButtonControlledCreator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControlledCreator : MonoBehaviour {
+
+	public GameObject thingToCreate;
+	public List<GameObject> things = new List<GameObject>();
+
+	public void Create(int count){
+		for (int i = 0; i < count; ++i) {
+			Vector3 p = transform.position;
+			p += Random.onUnitSphere;
+			p.z = 0;
+			GameObject obj = Instantiate (thingToCreate, p, transform.rotation);
+			things.Add (obj);
+		}
+	}
+
+	public void DestroyAll(){
+		for (int i = 0; i < things.Count; ++i) {
+			Destroy (things [i]);
+		}
+		things.Clear ();
+	}
+}
