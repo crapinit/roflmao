ea55e5bedbdd2ff7263874289586a01817db7e67
smorrissey <64bitart@gmail.com>
2017-09-26 09:05:50 -0700

    new map + player camera lock

---
+++ rofllmao/Assets/_PROJECT/scripts/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+	public float speed = .325f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float v = Input.GetAxis ("Vertical");
+		float h = Input.GetAxis ("Horizontal");
+		Vector2 dir = new Vector2 (h, v);
+		dir.Normalize ();
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+		rb.velocity = dir * speed;
+	}
+
+}
