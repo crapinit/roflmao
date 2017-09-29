69120227359d34e024a0c764ee6b7b24e9088a02
smorrissey <smorrissey18@shschools.org>
2017-09-29 07:55:24 -0700

    Began working on pathfinding for minions

---
+++ rofllmao/Assets/_PROJECT/scripts/PathFinding.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinding : MonoBehaviour {
+
+	public var targetObject = Transform;
+	private var navComponent = NavMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+
+		navComponent = this.transform.GetComponent (NavMeshAgent);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (victim) {
+		
+			navComponent.SetDestination (targetObject.position);
+		
+		}
+		
+	}
+}
